cd34fae2fa3f6f069cbe27300a29c3e179c18ee3
nicoargiz <nicoargiz@hotmail.com>
2023-08-02 08:06:41 -0300

    saludo

--- src/componentes/Home/Home.jsx
+++ src/componentes/Home/Home.jsx
@@ -7,7 +7,9 @@ const Home = () => {
       id="home"
       className="home-container flex flex-col justify-center items-center mb-5 mt-20 text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"
     >
-      <h1 className="h1">Bienvenido! Soy Nicolas Alonso y este es mi Portafolio.</h1>
+      <h1 className="h1">Bienvenido!<span role="img" aria-label="Saludo">
+        👋
+      </span>Soy Nicolas Alonso y este es mi Portafolio.</h1>
       <h3 className="h3">Full Stack Developer</h3>
     </section>
   );
